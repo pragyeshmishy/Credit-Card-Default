693182194a39e2bec2629239b32a835113964e0c
pragyeshmishy <67078326+pragyeshmishy@users.noreply.github.com>
2022-07-21 21:02:11 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="credit-card-fault-detection",
+    version="0.0.3",
+    description="ML project",
+    author="Ashutosh sharma",
+    packages=find_packages(),
+    license="MIT"
+)
